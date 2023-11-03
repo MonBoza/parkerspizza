648ee3d864da951f5baa23b7b90ed5af0b90c8d5
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2023-11-03 11:18:46 -0500

    Test 1 passed

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+function Pizza(toppings, size) {
+    this.toppings = toppings;
+    this.size = size;
+    this.currentOrder = 0;
+}
+
+
+
+
+
+
+
+//tests
+// let testPizza = new Pizza(["pepperoni", "mushrooms"], "large");

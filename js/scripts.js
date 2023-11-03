3ee0da17e06e963f14e27bda7ee58661a0407c68
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2023-11-03 14:15:55 -0500

    update index.html

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,8 @@ function Pizza(toppings, size) {
 
 Pizza.prototype.addPrice = function() {
     let currentPrice = 0;
-    let toppingTotal = 0;
-    for (let i = 0; i < this.toppings.length; i += 1) {
-        toppingTotal += 2;
+    for (let i = 1; i < this.toppings.length; i += 1) {
+        this.currentOrder = currentPrice += 2;
     } if (this.size === "large") {
         this.currentOrder = currentPrice += 15;
         } else if (this.size === "medium"){
@@ -17,18 +16,8 @@ Pizza.prototype.addPrice = function() {
         } else if (this.size === "small"){
             this.currentOrder = currentPrice += 8;
         } 
-        return this.currentOrder;
+        return this.currentOrder ;
 }
-
-Pizza.prototype.calculateToppings = function() {
-    let toppingTotal = 0;
-    for (let i = 0; i < this.toppings.length; i += 1) {
-        toppingTotal += 2;
-    }
-    return toppingTotal;
-}
-
-
 
 
 
